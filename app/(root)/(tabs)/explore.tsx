606152609f5bb34ba708f0746e1a088a52ac157e
rei2020-1 <canodemaj2002@gmail.com>
2025-10-05 17:20:55 +0200

    Implementing tabs navigation

--- app/(root)/(tabs)/explore.tsx
+++ app/(root)/(tabs)/explore.tsx
@@ -1,11 +1,12 @@
 import React from 'react'
-import { Text, View } from 'react-native'
+import { Text } from 'react-native'
+import { SafeAreaView } from 'react-native-safe-area-context'
 
 const Explore = () => {
   return (
-    <View>
+    <SafeAreaView>
       <Text>Explore</Text>
-    </View>
+    </SafeAreaView>
   )
 }
 
--- app/(root)/(tabs)/index.tsx
+++ app/(root)/(tabs)/index.tsx
@@ -1,4 +1,3 @@
-import { Link } from "expo-router";
 import { Text, View } from "react-native";
 
 export default function Index() {
@@ -12,19 +11,6 @@ export default function Index() {
     >
 
       <Text className="font-bold  my-10 font-rubik text-3xl">Welcome to the ReState</Text>
-
-      <Link href="/sign-in">
-        <Text>Sign In</Text>
-      </Link>
-      <Link href="/explore">
-        <Text>Explore</Text>
-      </Link>
-      <Link href="/profile">
-        <Text>Profile</Text>
-      </Link>
-      <Link href="/properties/1">
-        <Text>Property </Text>
-      </Link>
     </View>
   );
 }
